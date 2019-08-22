823ff67722b970f4066e284a3bee8d86d20965c0
SWittouck <stijn.wittouck@uantwerpen.be>
2019-08-22 10:50:46 +0200

    Infer pangenome of representatives with OrthoFinder and blast

---
+++ 04_analyze_representatives/05_cluster_genes.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+fin_scg_matrix_subset=../data_v3/genome_clusters/scg_matrix_subset.csv
+din_faas=../data_v3/genes/faas
+dout=../data_v3/representatives/orthofinder
+
+threads=16
+
+[[ -d $dout ]] || mkdir -p $dout
+
+# extract list of genome names from scg matrix
+genomes=$(cat $fin_scg_matrix_subset | cut -d , -f 1 | tail -n +2) 
+
+# copy genomes to OrthoFinder directory
+for genome in $genomes ; do
+  fin_faa=$(ls $din_faas/$genome*)
+  cp $fin_faa $dout
+done
+
+# unzip
+gunzip $dout/*.faa.gz
+
+orthofinder -M msa -os -t $threads -f $dout \
+  2>&1 | tee $dout/log_cluster_genes.txt
+
+# rezip
+gzip $dout/*.faa
