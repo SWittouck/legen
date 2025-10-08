7cf86aa4faa3faeef931ee6eb37c9756a58bbade
SWittouck <wittouck_stijn@hotmail.com>
2025-10-08 20:25:42 +0000

    Update dereplication

--- src/03_dereplicate_all/02_prepare_species.R
+++ src/03_dereplicate_all/02_prepare_species.R
@@ -1,106 +1,29 @@
 #!/usr/bin/env Rscript
 
-# This script creates one folder per species that has two or more genomes. It
-# splits the core100 genes, ffnpaths and faapaths per species and stores them in
-# these folders. It does all this for the full genome dataset, and then again
-# but only for genomes with a quality >= 90%.
+# This script splits the core gene table of all genomes into a core gene table
+# per species. 
 
-# dependencies: R v4.2.3, tidyverse v2.0.0
+# dependencies: R, tidyverse
 
 library(tidyverse)
 
-fin_genomes <- "../../results/all/genomes_metadata.csv"
-fin_ffnpaths <- "../../results/all/ffnpaths.txt"
-fin_faapaths <- "../../results/all/faapaths.txt"
-fin_core100_genes <- "../../results/all/core100/genes.tsv"
-dout_all <- "../../results/all/perspecies_all"
-dout_high_quality <- "../../results/all/perspecies_high_quality"
+fin_genes <- "../../results/all/core100/genes.tsv"
+fin_genomes <- "../../results/all/genomes.csv"
+dout <- "../../results/all/core100_perspecies"
 
-if (! dir.exists(dout_all)) dir.create(dout_all)
-if (! dir.exists(dout_high_quality)) dir.create(dout_high_quality)
+if (! dir.exists(dout)) dir.create(dout)
 
-# read the necessary data
-genomes <- read_csv(fin_genomes, col_types = cols())
-ffnpaths <- 
-  read_lines(fin_ffnpaths) %>%
-  tibble(ffnpath = ., genome = str_extract(., "[^/]+(?=.ffn.gz)"))
-faapaths <- 
-  read_lines(fin_faapaths) %>%
-  tibble(faapath = ., genome = str_extract(., "[^/]+(?=.faa.gz)"))
-core100_genes <- 
-  fin_core100_genes %>%
-  read_tsv(col_names = c("gene", "genome", "orthogroup"), col_types = cols())
+# read genome and gene tables
+colnames <- c("gene", "genome", "orthogroup")
+genes <- fin_genes %>% read_tsv(col_names = colnames, col_types = cols())
+genomes <- fin_genomes %>% read_csv(col_names = T, col_types = cols())
 
-# prepare genome dataset 1: genomes of species that have > 1 genome
-genomes_set1 <- 
-  genomes %>%
-  select(species, genome, quality) %>%
-  # only retain species with more than one genome
-  add_count(species, name = "n_genomes") %>%
-  filter(n_genomes > 1) %>% 
-  select(- n_genomes) %>%
-  # replace the space with an underscore in the species name
-  mutate(species = str_replace(species," ", "_")) %>%
-  # add ffnpaths
-  left_join(ffnpaths, by = "genome") %>%
-  # add faapaths
-  left_join(faapaths, by = "genome") %>%
-  # remove genomes with missing faa or ffn files
-  # (can happen if assemblies are suppressed on ncbi)
-  filter(! is.na(ffnpath))
-
-# prepare genome dataset 2: same as dataset 1, but only after quality filtering
-genomes_set2 <- 
-  genomes_set1 %>%
-  # retain only genomes with quality >= 0.90
-  filter(quality >= 0.90) %>%
-  # only retain species with more than one genome
-  add_count(species, name = "n_genomes") %>%
-  filter(n_genomes > 1) %>%
-  select(- n_genomes)
-
-# create folder per species and write ffnpaths and faapaths for dataset 1
-genomes_set1 %>%
-  # split table per species
-  split(f = .$species) %>%
-  # create folder per species and write ffnpaths and faapaths
-  walk2(., names(.), function(genomes, species) {
-    dout_sp <- paste0(dout_all, "/", species)
-    if (! dir.exists(dout_sp)) dir.create(dout_sp)
-    write_lines(genomes$ffnpath, paste0(dout_sp, "/ffnpaths.txt"))
-    write_lines(genomes$faapath, paste0(dout_sp, "/faapaths.txt"))
-  })
-
-# create folder per species and write ffnpaths and faapaths for dataset 2
-genomes_set2 %>%
-  # split table per species
-  split(f = .$species) %>%
-  # create folder per species and write ffnpaths and faapaths
-  walk2(., names(.), function(genomes, species) {
-    dout_sp <- paste0(dout_high_quality, "/", species)
-    if (! dir.exists(dout_sp)) dir.create(dout_sp)
-    write_lines(genomes$ffnpath, paste0(dout_sp, "/ffnpaths.txt"))
-    write_lines(genomes$faapath, paste0(dout_sp, "/faapaths.txt"))
-  })
-
-# write the core100 genes per species for dataset 1
-core100_genes %>%
-  right_join(genomes_set1, by = "genome") %>%
-  # create list with core100 genes per species
-  {split(.[, c("gene", "genome", "orthogroup")], .$species)} %>%
-  # write core100 genes to their respective species folders
-  walk2(., names(.), function(genes, species) {
-    dout_sp <- paste0(dout_all, "/", species)
-    write_tsv(genes, paste0(dout_sp, "/core100.tsv"), col_names = F)
-  })
-
-# write the core100 genes per species for dataset 2
-core100_genes %>%
-  right_join(genomes_set2, by = "genome") %>%
-  # create list with core100 genes per species
-  {split(.[, c("gene", "genome", "orthogroup")], .$species)} %>%
-  # write core100 genes to their respective species folders
-  walk2(., names(.), function(genes, species) {
-    dout_sp <- paste0(dout_high_quality, "/", species)
-    write_tsv(genes, paste0(dout_sp, "/core100.tsv"), col_names = F)
+# write core gene table per species
+genes %>%
+  left_join(select(genomes, genome, species), by = "genome") %>%
+  group_split(species) %>%
+  walk(~ {
+    species <- unique(.$species) %>% str_replace(" ", "_")
+    select(., gene, genome, orthogroup) %>%
+      write_tsv(paste0(dout, "/", species, ".tsv"), col_names = F)
   })
--- src/03_dereplicate_all/03_sample_perspecies.sh
+++ src/03_dereplicate_all/03_sample_perspecies.sh
@@ -1,27 +1,49 @@
 #!/usr/bin/env bash
 
-# For each species that has two genomes or more, this script samples a maximum
-# of 100 genomes, making sure that no two sampled genomes are more similar than
-# 99.99% (cANI90). 
+# For every species, this script samples a maximum of 100 genomes, making sure 
+# that no two sampled genomes are more similar than 99.99% (cANI90). 
 
-# dependencies: SCARAP v0.4.0
+# dependencies: SCARAP
 
-dio=../../results/all/perspecies_high_quality
+din_ffns=../../results/all/genes/ffns/
+din_core=../../results/all/core100_perspecies/
+fin_genomes=../../results/all/genomes.csv
+dout_derep=../../results/all/dereplication/
+dout_dereps=../../results/dereps/
 
-threads=32
+threads=16
+
+# create output folders
+[ -d $dout_derep ] || mkdir $dout_derep
+[ -d $dout_dereps ] || mkdir $dout_dereps
 
 # loop over species
-for dio_sp in $dio/* ; do
+for fin_core in $din_core/*.tsv ; do
 
-  echo $dio_sp
-  
+  species=$(basename $fin_core .tsv)
+
+  echo $species
+
   # continue loop if sampling output already exists
-  if [ -f $dio_sp/sample/seeds.txt ] ; then continue ; fi
-  
+  if [ -f $dout_derep/$species/seeds.txt ] ; then continue ; fi
+
   # perform sampling
-  scarap sample $dio_sp/ffnpaths.txt $dio_sp/core100.tsv \
-    $dio_sp/sample -i 0.9999 -m 100 --method mean90 -t $threads
-    
+  scarap sample $din_ffns $fin_core $dout_derep/$species \
+    -i 0.9999 -m 100 --method mean90 -t $threads
+
   echo ""
-  
+
 done
+
+# concatenate all the selected genomes
+cat $dout_derep/*/seeds.txt > $dout_dereps/accessions.txt
+
+# subset genome metadata to dereps
+awk -F ',' 'NR == FNR {a[$1]; next} FNR == 1 || $1 in a' \
+  $dout_dereps/accessions.txt $fin_genomes > $dout_dereps/genomes.csv
+
+# create genome table with only the species
+tail -n +2 $dout_dereps/genomes.csv |\
+  cut -d ',' -f 1,12 |\
+  tr ',' '\t' >\
+  $dout_dereps/genomes_species.tsv
