c76f5882b78021c71b9b24c41ec16b09cbbea2d7
SWittouck <wittouck_stijn@hotmail.com>
2025-10-08 10:02:13 +0000

    Update core gene identification

--- src/03_dereplicate_all/01_extract_core.sh
+++ src/03_dereplicate_all/01_extract_core.sh
@@ -1,19 +1,28 @@
-# This script identifies the top 100 single-copy core genes in all genomes of 
-# Lactobacillales. 
+#!/usr/bin/env bash
 
-# dependencies: SCARAP v0.4.0 (bde4a13)
+# This script identifies the top 100 single-copy core genes in representative
+# genomes of species of Lactobacillales and extracts them from all genomes. 
 
-fin_faapaths_repr=../../results/representatives/faapaths.txt
-fin_faapaths_all=../../results/all/faapaths.txt
-dout_repr=../../results/representatives
+# dependencies: SCARAP
+
+din_faas=../../results/all/genes/faas/
+fin_acc_repr=../../results/speciesreps/accessions.txt
+fin_acc_all=../../results/all/accessions.txt
+fin_pan_repr=../../results/speciesreps/pangenome/pangenome.tsv
+
+dout_repr=../../results/speciesreps
 dout_all=../../results/all
 
-threads=32
+threads=16
 
-# build a profile database of the top 100 single-copy core genes 
-scarap build $fin_faapaths_repr $dout_repr/pangenome/pangenome.tsv \
-  $dout_repr/core100 -p 0.9 -m 100 -t $threads
+# build a profile database of the top 100 single-copy core genes
+sed "s|^|$din_faas|; s|\$|.faa.gz|" $fin_acc_repr > $dout_repr/faapaths.txt
+scarap build $dout_repr/faapaths.txt $fin_pan_repr $dout_repr/core100 \
+  -p 0.9 -m 100 -t $threads
+rm $dout_repr/faapaths.txt
   
 # identify the top 100 core genes in all genomes 
-scarap search $fin_faapaths_all $dout_repr/core100 $dout_all/core100 \
+sed "s|^|$din_faas|; s|\$|.faa.gz|" $fin_acc_all > $dout_all/faapaths.txt
+scarap search $dout_all/faapaths.txt $dout_repr/core100 $dout_all/core100 \
   -t $threads
+rm $dout_all/faapaths.txt
